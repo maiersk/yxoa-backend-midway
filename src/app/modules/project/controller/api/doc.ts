f2c7ab11b218d41321deb5d098d7ef1552837d76
maiersk <poilyoungman@foxmail.com>
2021-12-27 18:04:17 +0800

    update: project api and user entity

--- src/app/modules/project/controller/api/doc.ts
+++ src/app/modules/project/controller/api/doc.ts
@@ -1,6 +1,10 @@
-import { Post, Provide } from '@midwayjs/decorator';
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
+import { Body, Inject, Post, Provide, Query } from '@midwayjs/decorator';
 import { CoolController, BaseController } from '@cool-midway/core';
 import { ProjectAppDocEntity } from '../../entity/doc';
+import { ProjectAppService } from '../../service/project';
+import { ProjectAppDocService } from '../../service/doc';
 
 /**
  * 文档对外功能 api
@@ -11,10 +15,22 @@ import { ProjectAppDocEntity } from '../../entity/doc';
   entity: ProjectAppDocEntity,
 })
 export class DocApiController extends BaseController {
-  @Post('/generate')
-  async generate() {
+  @Inject()
+  docService: ProjectAppDocService;
+  @Inject()
+  projectService: ProjectAppService;
+
+  @Post('/generate', { summary: '生成文档' })
+  async generate(@Query() id: number, @Body() fields: any) {
     try {
-      
+      const doc = await this.docService.find(id)
+
+      if (!doc) { this.ok({ message: '找不到该id实例' }) }
+      if (!doc.templateFile) { this.ok({ message: '找不到模板文件' }) }
+      let filepath, file
+      filepath = resolve(__dirname, '../files/temp', doc.templateFile)
+      file = await readFileSync(filepath)
+
       return this.ok()
     } catch (err) {
       throw new Error(err.message)
---
+++ src/app/modules/project/entity/project_user.ts
@@ -0,0 +1,15 @@
+import { EntityModel } from '@midwayjs/orm';
+import { BaseEntity } from '@cool-midway/core';
+import { Column } from 'typeorm';
+
+/**
+ * 项目参与人
+ */
+@EntityModel('project_app_users')
+export class ProjectAppUserEntity extends BaseEntity {
+  @Column({ comment: '项目Id' })
+  projectId: string;
+
+  @Column({ comment: '用户Id' })
+  userId: string;
+}
